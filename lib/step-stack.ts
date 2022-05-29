b65e12ba86c81f51ea5e0e735f99352dd49f2a53
gensobunya <gen@gensobunya.net>
2022-05-29 15:20:32 +0900

    原型

--- lib/step-stack.ts
+++ lib/step-stack.ts
@@ -1,16 +1,28 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import {
+  Stack,
+  StackProps,
+  aws_lambda as lambda,
+  aws_stepfunctions as sfn,
+  aws_stepfunctions_tasks as tasks,
+  Duration,
+} from "aws-cdk-lib";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { Construct } from "constructs";
 
 export class StepStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const hellofunction = new NodejsFunction(this, "helloWorld", {
+      entry: "lambda/splitwise_get_expenses.ts",
+      handler: "get",
+      timeout: Duration.seconds(10),
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'StepQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const stateMachine = new sfn.StateMachine(this, "MyStateMachine", {
+      definition: new tasks.LambdaInvoke(this, "MyLambdaTask", {
+        lambdaFunction: hellofunction,
+      }).next(new sfn.Succeed(this, "GreetedWorld")),
+    });
   }
 }
