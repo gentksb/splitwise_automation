41b012cb130af91b5dc04b998bf787139b6986aa
gensobunya <gen@gensobunya.net>
2022-05-30 19:47:00 +0900

    update function

--- lambda/splitwise_get_expenses.ts
+++ lambda/splitwise_get_expenses.ts
@@ -4,15 +4,23 @@ import {
   APIGatewayEvent,
   Handler,
 } from "aws-lambda";
+import axios from "axios";
+import { get } from "https";
 
 export const handler: Handler = async (
   event: APIGatewayEvent,
   context: Context
 ): Promise<APIGatewayProxyResult> => {
-  console.log(`Event: ${JSON.stringify(event, null, 2)}`);
-  console.log(`Context: ${JSON.stringify(context, null, 2)}`);
-
   const { SPLITWISE_API_KEY_PARAMETER_NAME } = process.env;
+
+  const getApiResponse = await axios.get(
+    "https://secure.splitwise.com/api/v3.0/get_current_user",
+    {
+      headers: { Authorization: `Bearer ${SPLITWISE_API_KEY_PARAMETER_NAME}` },
+    }
+  );
+
+  console.log(JSON.stringify(getApiResponse.data));
 
   return {
     statusCode: 200,
