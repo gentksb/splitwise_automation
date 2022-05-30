d7bf228218136b5612e65342d74271b2c01386c8
gensobunya <gen@gensobunya.net>
2022-05-30 20:57:39 +0900

    判定

--- lambda/splitwise_get_expenses.ts
+++ lambda/splitwise_get_expenses.ts
@@ -5,7 +5,6 @@ import {
   Handler,
 } from "aws-lambda";
 import axios from "axios";
-import { get } from "https";
 
 export const handler: Handler = async (
   event: APIGatewayEvent,
@@ -14,18 +13,38 @@ export const handler: Handler = async (
   const { SPLITWISE_API_KEY_PARAMETER_NAME } = process.env;
 
   const getApiResponse = await axios.get(
-    "https://secure.splitwise.com/api/v3.0/get_current_user",
+    "https://secure.splitwise.com/api/v3.0/get_expenses",
     {
       headers: { Authorization: `Bearer ${SPLITWISE_API_KEY_PARAMETER_NAME}` },
     }
   );
 
-  console.log(JSON.stringify(getApiResponse.data));
+  const isSharedCost = (expense: any) => {
+    const { cost, users } = expense;
+    const splitRate = parseInt(users[0].owed_share) / parseInt(cost);
+
+    return splitRate !== 0 && splitRate !== 1;
+  };
+
+  const expensesList: Array<any> = getApiResponse.data.expenses;
+
+  const noPaymentExpenses = await expensesList.filter(
+    (expense) => expense.payment === false
+  );
+
+  const willSplitExpenses = await noPaymentExpenses.filter((expense) =>
+    isSharedCost(expense)
+  );
+
+  const logMessage =
+    expensesList.length === 0
+      ? "取得対象の精算経費がありません"
+      : `直近${expensesList.length}の経費のうち、${noPaymentExpenses.length}件が未清算、${willSplitExpenses.length}件を割り勘処理します`;
 
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: "hello world",
+      message: logMessage,
     }),
   };
 };
