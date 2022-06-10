264ad69ab925e70f916333ee620b2aaeff67288e
gensobunya <gen@gensobunya.net>
2022-06-10 22:36:37 +0900

    bugfix

--- lambda/logic/splitExpense.ts
+++ lambda/logic/splitExpense.ts
@@ -1,7 +1,7 @@
 export const splitExpense = (expense: any) => {
   const { USER1_RATE, USER2_RATE, USER1_ID } = process.env;
   const numCost = parseInt(expense.cost);
-  const payerId = expense.repayments[0].to;
+  const payerId = expense.repayments[0].to.toString();
 
   if (USER1_RATE != null && USER2_RATE != null && USER1_ID !== null) {
     const payerOwedShare =
